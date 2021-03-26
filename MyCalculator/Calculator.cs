044f70b316a6f63bd23eb0e2bc22f43d5e072850
Blare Robinson <berobi01@louisville.edu>
2021-03-26 05:45:25 -0400

    Create methods based off operands, that return values.

--- MyCalculator/Calculator.cs
+++ MyCalculator/Calculator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace MyCalculator
 {
     public class Calculator
@@ -9,7 +10,7 @@ namespace MyCalculator
             string Input1_String, Input2_String, Operator;
             double Input1_Num, Input2_Num;
             double result = 0;
-
+            
             Console.Write("Please enter your first number:");
             Input1_String = Console.ReadLine();
             Input1_Num = Convert.ToDouble(Input1_String);
@@ -21,51 +22,73 @@ namespace MyCalculator
             Console.Write("Please enter operator you want to perform: (+,-,*,/):)");
             Operator = Console.ReadLine();
 
-            result = Output(Operator, Input1_Num, Input2_Num, result);
+            result = Add(Operator, Input1_Num, Input2_Num, result);
+            result = Subtract(Operator, Input1_Num, Input2_Num, result);
+            result = Divide(Operator, Input1_Num, Input2_Num, result);
+            result = Multiply(Operator, Input1_Num, Input2_Num, result);
 
             Console.WriteLine("{0} {1} {2} = {3}", Input1_Num, Operator, Input2_Num, result);
             Console.ReadLine();
         }
 
-        public double Add(string Operator, double Input1_Num, double Input2_Num, double result)
-        {
-            result = Input1_Num + Input2_Num;
-            return result;
-        }
 
-        public double Subtract(string Operator, double Input1_Num, double Input2_Num, double result)
-        {
-            result = Input1_Num - Input2_Num;
-            return result;
-        }
-
-        public double Divide(string Operator, double Input1_Num, double Input2_Num, double result)
-        {
-            result = Input1_Num / Input2_Num;
-            return result;
-        }
-
-        public double Multiply(string Operator, double Input1_Num, double Input2_Num, double result)
-        {
-            result = Input1_Num * Input2_Num;
-            return result;
-        }
-
-        private static double Output(string Operator, double Input1_Num, double Input2_Num, double result)
+        
+        public static double Add(string Operator, double Input1_Num, double Input2_Num, double result)
         {
             if (Operator == "+")
+            {
                 result = Input1_Num + Input2_Num;
-            else if (Operator == "-")
-                result = Input1_Num - Input2_Num;
+            }
 
-            else if (Operator == "/")
-                result = (double)Input1_Num / Input2_Num;
-
-            else if (Operator == "*")
-                result = Input1_Num * Input2_Num;
             return result;
         }
 
+        public static double Subtract(string Operator, double Input1_Num, double Input2_Num, double result)
+        {
+            if (Operator == "-")
+            {
+                result = Input1_Num - Input2_Num;
+            }
+
+            return result;
+        }
+
+        public static double Divide(string Operator, double Input1_Num, double Input2_Num, double result)
+        {
+            if (Operator == "/")
+            {
+                result = Input1_Num / Input2_Num;
+            }
+
+            return result;
+        }
+
+        public static double Multiply(string Operator, double Input1_Num, double Input2_Num, double result)
+        {
+            if (Operator == "*")
+            {
+                result = Input1_Num * Input2_Num;
+            }
+
+            return result;
+        }
+
+        //private static double Output(string Operator, double Input1_Num, double Input2_Num, double result)
+        //{
+            //if (Operator == "+")
+            //{
+            //    result = Input1_Num + Input2_Num;
+            //}
+            //else if (Operator == "-")
+            //    result = Input1_Num - Input2_Num;
+
+            //else if (Operator == "/")
+            //    result = (double)Input1_Num / Input2_Num;
+
+            //else if (Operator == "*")
+            //    result = Input1_Num * Input2_Num;
+            //return result;
+        //}
     }
 
 }
