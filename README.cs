ffb893944d3ccef37276a96517bc3ba9e38a3760
Blare Robinson <berobi01@louisville.edu>
2021-03-31 11:30:48 -0400

    Update Readme with features i use.

--- README.cs
+++ README.cs
@@ -1,1 +1,4 @@
 ﻿// This project is a calculator that does addtion, subtraction, division and mulitplication. It requests two interger inputs from the user and then ask them to select an operator to perform. It then returns the correct value.
+// For the requirements I did the following:
+// 1) Created a conversion tool that converts intergers to decimals.
+// 2) Created a subclass basicCalculator, that inherits properties from Calculator.
