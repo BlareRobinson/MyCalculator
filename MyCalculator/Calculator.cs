e943b84dae7999fe4599faa04d4df23953a2610f
Blare Robinson <berobi01@louisville.edu>
2021-03-31 15:25:34 -0400

    Add a coversion method.

--- MyCalculator/Calculator.cs
+++ MyCalculator/Calculator.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 
 namespace MyCalculator
 {
     public class Calculator
     {
+        
+
         public Calculator()
         {
-            string Input1_String, Input2_String, Operator;
+            string Input1_String, Input2_String, Operator, ouncesConverted;
             double Input1_Num, Input2_Num;
             double result = 0;
-            
+            double numberOfCups = 0;
+
+
             Console.Write("Please enter your first number:");
             Input1_String = Console.ReadLine();
             Input1_Num = Convert.ToDouble(Input1_String);
@@ -22,17 +27,25 @@ namespace MyCalculator
             Console.Write("Please enter operator you want to perform: (+,-,*,/):)");
             Operator = Console.ReadLine();
 
+            Console.WriteLine("How many cups would you like to convert into ounces?");
+            ouncesConverted = Console.ReadLine();
+
+
+
             result = Add(Operator, Input1_Num, Input2_Num, result);
             result = Subtract(Operator, Input1_Num, Input2_Num, result);
             result = Divide(Operator, Input1_Num, Input2_Num, result);
-            result = Multiply(Operator, Input1_Num, Input2_Num, result);
+            result = Multiply(Operator, Input1_Num, Input2_Num, result);     
+            result = Conversion(numberOfCups);
+
+
 
             Console.WriteLine("{0} {1} {2} = {3}", Input1_Num, Operator, Input2_Num, result);
             Console.ReadLine();
+
         }
 
 
-        
         public static double Add(string Operator, double Input1_Num, double Input2_Num, double result)
         {
             if (Operator == "+")
@@ -73,22 +86,13 @@ namespace MyCalculator
             return result;
         }
 
-        //private static double Output(string Operator, double Input1_Num, double Input2_Num, double result)
-        //{
-            //if (Operator == "+")
-            //{
-            //    result = Input1_Num + Input2_Num;
-            //}
-            //else if (Operator == "-")
-            //    result = Input1_Num - Input2_Num;
+        public static double Conversion(double numberOfCups)
+        {
+            double ouncesConverted = numberOfCups * 8;
+            return ouncesConverted;
+        }
 
-            //else if (Operator == "/")
-            //    result = (double)Input1_Num / Input2_Num;
-
-            //else if (Operator == "*")
-            //    result = Input1_Num * Input2_Num;
-            //return result;
-        //}
     }
+
 
 }
